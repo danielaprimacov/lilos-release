c48e2eeb74ac53d957c1996f43d884a5c669843c
Daniela <daniela.primacov@yahoo.com>
2025-02-27 23:10:38 +0100

    Fixing the arrow movement

--- js/player.js
+++ js/player.js
@@ -7,7 +7,9 @@ class Player {
     this.height = height;
     this.directionX = 0;
     this.directionY = 0;
-    this.arrowRotation = -90;
+    this.arrowAngle = -90;
+    this.arrowDirection = 1;
+    this.arrowRotation = 0;
 
     this.element = document.createElement("img");
     this.element.src = imageURL;
@@ -21,46 +23,14 @@ class Player {
     this.gameScreen.appendChild(this.element);
 
     // Create the arrow element
-    this.arrowElement = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "svg"
-    );
-    this.arrowElement.setAttribute("width", "150");
-    this.arrowElement.setAttribute("height", "50");
-    this.arrowElement.style.display = "absoulte";
-
-    // Create the arrow line
-    let arrowLine = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "line"
-    );
-    arrowLine.setAttribute("x1", "10");
-    arrowLine.setAttribute("y1", "40");
-    arrowLine.setAttribute("x2", "90");
-    arrowLine.setAttribute("y2", "40");
-    arrowLine.setAttribute("stroke", "black");
-    arrowLine.setAttribute("stroke-width", "2");
-
-    // Make the line dashed
-    arrowLine.setAttribute("stroke-dasharray", "10,5");
-
-    // Create the arrowhead
-    let arrowHead = document.createElementNS(
-      "http://www.w3.org/2000/svg",
-      "polygon"
-    );
-    arrowHead.setAttribute("points", "90,40 80,35 80,45");
-    arrowHead.setAttribute("fill", "black");
-
-    // Rotate the arrow
-    this.arrowElement.setAttribute(
-      "transform",
-      `translate(150, 300) rotate(${this.arrowRotation})`
-    );
+    let arrowElement = document.createElement("div");
+    arrowElement.classList.add("arrow");
+    arrowElement.style.top = `${this.top - 80}px`;
+    arrowElement.style.left = `${this.left + this.width / 2 - 105}px`;
+    arrowElement.style.transform = `rotate(${this.arrowRotation}deg)`;
+    this.arrowElement = arrowElement;
 
     // Append to the arrow element
-    this.arrowElement.appendChild(arrowLine);
-    this.arrowElement.appendChild(arrowHead);
     this.gameScreen.appendChild(this.arrowElement);
 
     this.updatePosition();
@@ -69,27 +39,22 @@ class Player {
   updatePosition() {
     this.element.style.left = `${this.left}px`;
     this.element.style.top = `${this.top}px`;
-
     this.updateArrowPosition();
   }
 
   updateArrowPosition() {
-    const rotationOriginX = this.left + this.width / 2 - 94;
-    const rotationOriginY = this.top - 85;
-
-    this.arrowElement.setAttribute(
-      "transform",
-      `translate(${rotationOriginX}, ${rotationOriginY}) rotate(${this.arrowRotation}) `
-    );
+    this.arrowElement.style.top = `${this.top - 80}px`;
+    this.arrowElement.style.left = `${this.left + this.width / 2 - 105}px`;
+    this.arrowElement.style.transform = `rotate(${this.arrowRotation}deg)`;
   }
 
   move() {}
 
   moveArrow(key) {
-    // Set the limits of the line rotation
-    const minRotation = -135;
-    const maxRotation = -45;
-    const rotationStep = 5;
+    //Set the limits of the line rotation
+    const minRotation = -55;
+    const maxRotation = 55;
+    const rotationStep = 10;
     if (key === "left" && this.arrowRotation > minRotation) {
       this.arrowRotation -= rotationStep;
     } else if (key === "right" && this.arrowRotation < maxRotation) {
