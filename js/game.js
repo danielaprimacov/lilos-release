0bf8db6072b2bd37834f19b4cd7ae5e9dc51d6cb
Daniela <daniela.primacov@yahoo.com>
2025-03-08 03:16:40 +0100

    Define the Modal Window (How to Play)

--- js/game.js
+++ js/game.js
@@ -48,7 +48,7 @@ class Game {
 
     if (!this.modalScreen) {
       this.modalScreen = document.getElementById("modal-screen");
-      this.modalScreen.innerText = "Get Ready";
+      this.createHowToPlayModal();
     }
 
     // Fade out the game intro smoothly
@@ -197,5 +197,48 @@ class Game {
         window.location.reload();
       }, 1000);
     }
+  }
+
+  createHowToPlayModal() {
+    const explanation = document.createElement("div");
+    const explanationTitle = document.createElement("h1");
+    const explanationParagraph = document.createElement("p");
+    const goalParagraph = document.createElement("p");
+    const actions = document.createElement("div");
+    const leftArrow = document.createElement("img");
+    const rightArrow = document.createElement("img");
+    const spaceBar = document.createElement("img");
+
+    actions.id = "how-to-play-actions";
+
+    leftArrow.src = "../images/keyboard-key-left-arrow.png";
+    leftArrow.alt = "How to play - left arrow";
+    leftArrow.classList.add("keyboard-arrow");
+
+    rightArrow.src = "../images/keyboard-key-right-arrow.png";
+    rightArrow.alt = "How to play - right arrow";
+    rightArrow.classList.add("keyboard-arrow");
+
+    spaceBar.src = "../images/space-key.png";
+    spaceBar.alt = "How to play - space bar";
+    spaceBar.classList.add("keyboard-space");
+
+    actions.appendChild(leftArrow);
+    actions.appendChild(spaceBar);
+    actions.appendChild(rightArrow);
+
+    explanationTitle.textContent = "How to Play";
+    explanationParagraph.textContent =
+      "Use the left and right arrow keys to move the player and use the space bar to jump.";
+
+    goalParagraph.textContent =
+      "The goal is to defeat the enemy by reducing his health to 0.The player and enemy health bars are visible above. To rescue Lilo, you must empty the enemy's health bar!";
+
+    explanation.appendChild(explanationTitle);
+    explanation.appendChild(explanationParagraph);
+    explanation.appendChild(actions);
+    explanation.appendChild(goalParagraph);
+
+    this.modalScreen.appendChild(explanation);
   }
 }
